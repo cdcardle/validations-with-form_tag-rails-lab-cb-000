8711afaf29937103ec372e34db28c30894abb36d
Chris Cardle <cdcardle@gmail.com>
2018-06-01 12:16:26 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :authors
-  resources :posts
+  resources :authors, only: [:show, :new, :create]
+  resources :posts, only: [:show, :edit, :update]
 end
